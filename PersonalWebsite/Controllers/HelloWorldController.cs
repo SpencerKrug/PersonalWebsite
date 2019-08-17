0c39af2e9535fb8f31214a7c555beadd03fb0a6d
Spencer-PC\Spencer <spncrkrug@gmail.com>
2019-08-17 17:42:31 -0400

    Finished controller section. Left off at link below.
    https://docs.microsoft.com/en-us/aspnet/mvc/overview/getting-started/introduction/adding-a-view

---
+++ PersonalWebsite/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalWebsite.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public String Index()
+        {
+            return "This is my <b>default</b> acttion...";
+        }
+
+        //GET: /HelloWorld/Welcome/
+        public string Welcome(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID); 
+        }
+    }
+}
