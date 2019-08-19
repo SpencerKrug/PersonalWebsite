47f8ef8fd96e6324b3e740397ee0b2f02b2a53a2
Spencer-PC\Spencer <spncrkrug@gmail.com>
2019-08-19 19:14:57 -0400

    Finished the controller section.
    /HelloWorld/Welcome?name=Scott&numtimes=4

--- PersonalWebsite/Controllers/HelloWorldController.cs
+++ PersonalWebsite/Controllers/HelloWorldController.cs
@@ -9,15 +9,18 @@ namespace PersonalWebsite.Controllers
     public class HelloWorldController : Controller
     {
         // GET: /HelloWorld/
-        public String Index()
+        public ActionResult Index()
         {
-            return "This is my <b>default</b> acttion...";
+            return View();
         }
 
         //GET: /HelloWorld/Welcome/
-        public string Welcome(string name, int ID = 1)
+        public ActionResult Welcome(string name, int numTimes = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID); 
+            ViewBag.Message = "Hello" + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View(); 
         }
     }
 }
