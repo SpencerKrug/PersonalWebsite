d8dbe6f2ceac6ca268487cf50c797ffec34f3b3c
Specer Krug <spncrkrug@gmail.com>
2019-10-24 18:55:50 -0400

    JavaShapes lab can be downloaded and additions were made to the contact page.

--- PersonalWebsite/Controllers/ContactController.cs
+++ PersonalWebsite/Controllers/ContactController.cs
@@ -13,5 +13,13 @@ namespace PersonalWebsite.Controllers
         {
             return View();
         }
+
+        public ActionResult Download()
+        {
+            //First parameter is the path to the file.
+            //Second parameter is the type of file.
+            //Third parameter is the name of the file.
+            return File(Server.MapPath("~/Content/JavaShapes.zip"), "application/zip", "JavaShapes.zip");
+        }
     }
 }
