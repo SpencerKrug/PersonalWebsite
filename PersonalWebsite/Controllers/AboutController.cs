3d906107204dcdc5234a330bdbb22edbdd9ee80a
Spencer Krug <spncrkrug@gmail.com>
2019-11-25 18:03:18 -0500

    Almost done making about view mobile friendly.

--- PersonalWebsite/Controllers/AboutController.cs
+++ PersonalWebsite/Controllers/AboutController.cs
@@ -20,5 +20,14 @@ namespace PersonalWebsite.Controllers
                 return View();
             }
         }
+
+        public ActionResult Download()
+        {
+            //First parameter is the path to the file.
+            //Second parameter is the type of file.
+            //Third parameter is the name of the file.
+            //https://www.wiley.com/legacy/compbooks/graham/html4ed/appb/mimetype.html
+            return File(Server.MapPath("~/Content/(Resume)SpencerKrug10-02-2019 .pdf"), "application/pdf", "(Resume)SpencerKrug10-02-2019 .pdf");
+        }
     }
 }
